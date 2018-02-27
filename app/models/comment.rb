8ebf70c3d76a5a61b09fe3691e00a2f00f079b7c
s11rikuya <rikuya.suzuki@gaiax.com>
2018-02-27 11:55:37 +0900

    バリデーション

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  validates :name, {presence: true}
+  validates :text, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226162949) do
+ActiveRecord::Schema.define(version: 20180227022903) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180226162949) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "text"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
