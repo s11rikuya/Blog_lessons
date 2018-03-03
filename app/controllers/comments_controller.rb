a69884ae410d7d11e29d3cfb7de4d5d7342ec593
s11rikuya <rikuya.suzuki@gaiax.com>
2018-03-03 16:48:10 +0900

    render問題解決

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(name: params[:name], text: params[:text], post_id: params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
